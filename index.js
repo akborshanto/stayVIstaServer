70daf4b156f70a36a235c48c1367892682a4f27c
akbor shanto <akborshanto2002@gmail.com>
2024-05-26 01:02:43 -0700

    email with room add

--- index.js
+++ index.js
@@ -1,40 +1,42 @@
-const express = require('express')
-const app = express()
-require('dotenv').config()
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
-const jwt = require('jsonwebtoken')
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const jwt = require("jsonwebtoken");
 
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 5000;
 
 // middleware
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  origin: ["http://localhost:5173", "http://localhost:5174"],
   credentials: true,
   optionSuccessStatus: 200,
-}
-app.use(cors(corsOptions))
+  // methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  // allowedHeaders: ['Content-Type', 'Authorization'],
+};
+app.use(cors(corsOptions));
 
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
 
 // Verify Token Middleware
 const verifyToken = async (req, res, next) => {
-  const token = req.cookies?.token
-  console.log(token)
+  const token = req.cookies?.token;
+  console.log(token);
   if (!token) {
-    return res.status(401).send({ message: 'unauthorized access' })
+    return res.status(401).send({ message: "unauthorized access" });
   }
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
     if (err) {
-      console.log(err)
-      return res.status(401).send({ message: 'unauthorized access' })
+      console.log(err);
+      return res.status(401).send({ message: "unauthorized access" });
     }
-    req.user = decoded
-    next()
-  })
-}
+    req.user = decoded;
+    next();
+  });
+};
 
 //const uri = `mongodb+srv://}:${process.env.DB_PASS}@main.mq0mae1.mongodb.net/?retryWrites=true&w=majority&appName=Main`
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.phei2xm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1`;
@@ -44,7 +46,7 @@ const client = new MongoClient(uri, {
     strict: true,
     deprecationErrors: true,
   },
-})
+});
 
 /* async function run() {
   try {
@@ -85,81 +87,89 @@ const client = new MongoClient(uri, {
 }
  */
 
-
-
-
-
-
-    
-
-    /* get the data from  */
-
-
-
-
-
-
-    
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-/* const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-}); */
-
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
 
-const roomCollection=client.db('stayvista').collection('room')
+    const roomCollection = client.db("stayvista").collection("room");
 
-    app.get('/rooms',async(req,res)=>{
-      
-      const category=req.query.category;
-      let query={}
-      console.log(category)
+    app.get("/rooms", async (req, res) => {
+      const category = req.query.category;
+      let query = {};
+      console.log(category);
 
-      if(category && category !== "null") query = {category}
-      const result=await roomCollection.find(query).toArray()
-      res.send(result)
-      
-          })
-/* get the singe room data from roomCollection  */
-app.get("/room/:id",async(req,res)=>{
+      if (category && category !== "null") query = { category };
+      const result = await roomCollection.find(query).toArray();
+      res.send(result);
+    });
+    /* get the singe room data from roomCollection  */
+    app.get("/room/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const consequence = await roomCollection.findOne(query);
+      res.send(consequence);
+    });
+
+
+
+/* POST THE DATA  In ROOOM COOLECTION DB  */
+app.post('/room',async (req,res)=>{
+const query= req.body;
+console.log(query)
+const result= await roomCollection.insertOne(query);
+res.send(result)
+
+
+
+})
+/* Get The my listing data specipic uerer */
+
+app.get('/my-listings/:email',async (req,res)=>{
+
+const email= req.params.email || "";
+// console.log(email)
+const query= {'host.email': email}
+// console.log(query)
+const resut= await roomCollection.find(query).toArray()
+res.send(resut)
+
+
+})
+
+/*DELETE The my listing data specipic uerer */
+app.delete('/my-List/:id',async(req,res)=>{
 
 const id=req.params.id;
 const query={_id: new ObjectId(id)}
-const consequence=await roomCollection.findOne(query)
-res.send(consequence)
 
+const result= await roomCollection.deleteOne(query)
+res.send(result)
 
 })
 
 
 
-   // await client.connect();
+
+    // await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } 
-  
-  finally {
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
     // Ensures that the client will close when you finish/error
-  //  await client.close();
+    //  await client.close();
   }
 }
 //run().catch(console.dir);
 
-    // Send a ping to confirm a successful connection
-run().catch(console.dir)
+// Send a ping to confirm a successful connection
+run().catch(console.dir);
 
-app.get('/', (req, res) => {
-  res.send('Hello from StayVista Server..')
-})
+app.get("/", (req, res) => {
+  res.send("Hello from StayVista Server..");
+});
 
 app.listen(port, () => {
-  console.log(`StayVista is running on port ${port}`)
-})
+  console.log(`StayVista is running on port ${port}`);
+});
